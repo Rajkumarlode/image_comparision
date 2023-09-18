5321f0d4a85f600f54c4191cf5b07ff65f5b8d4b
Rajkumarlode <79139052+Rajkumarlode@users.noreply.github.com>
2023-09-17 21:59:37 -0800

    Update imagecomparision.py

--- imagecomparision.py
+++ imagecomparision.py
@@ -4,26 +4,31 @@ import numpy as np
 from PIL import Image,ImageChops
 
 from google.colab.patches import cv2_imshow
+#using opps concept  in python
+#it's a demo_code of comparision  of gray scale image with original one
 
-imagePaths = list(paths.list_images('/content/photos'))
-
-for imagePath in imagePaths:
-  image = cv2.imread(imagePath)
-  gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-  def mse(imagePaths, gray):
-    h,w = imagePaths.shape
-    diff = cv2.subtract(imagePaths, gray)
-    err = np.sum(diff**2)
-    mse = err/(float(h*w))
-    return mse
-  def any_diff():
-    imagepaths,gray=Image.open('imagePaths'),Image.open('gray)')
-    diff=ImageChops.difference(imagePaths,gray)
-    if diff.getbbox():
-      diff.show()
-    else:
-      print("no difference")
-  print(mse.__doc__)
-  print(any_diff.__doc__)
-  #cv2_imshow(gray)
-
+class imagecomparison:
+    imagePaths = list(paths.list_images('/content/photos'))
+    def __init__(self):
+        for imagePath in imagePaths:
+            image = cv2.imread(imagePath)
+            gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+            def mse(imagePaths, gray):
+                h,w = imagePaths.shape
+                diff = cv2.subtract(imagePaths, gray)
+                err = np.sum(diff**2)
+                mse = err/(float(h*w))
+                return mse
+            def any_diff():
+                imagepaths,gray=Image.open('imagePaths'),Image.open('gray)')
+                diff=ImageChops.difference(imagePaths,gray)
+                if diff.getbbox():
+                     diff.show()
+                else:
+                     print("no difference")
+            print(mse)
+            print(any_dif)
+#imagecomparison()
+obj1=imagecomparision()
+print(obj1.mse())
+    
